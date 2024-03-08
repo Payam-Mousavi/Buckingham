f0f33fa8461cde75edd21e2e2f0aa041e110d81e
Payam Mousavi <payam.mousavi@amii.ca>
2024-03-08 11:46:01 -0800

    Clean up and saving of figures for Model_0.

--- eval.py
+++ eval.py
@@ -22,16 +22,21 @@ def eval_model(dataset, model_path, visualize=False):
     mse = mean_squared_error(ground_truths, predictions)
     mae = mean_absolute_error(ground_truths, predictions)
 
+    model_name = model_path.split('/')[-1].split('.')[0]
+    figure_save_path = f'Figures/{model_name}_CD_Eval.png'
+
     if visualize:
         plt.scatter(Re_vec, predictions, color='red', label='CD Predicted', marker='x', alpha=0.3)
         plt.scatter(Re_vec, ground_truths, color='blue', label='CD True', marker='o', alpha=0.3)
         # plt.scatter(Re_vec, np.abs(predictions - ground_truths), color='green', label='CD Error', marker='.', alpha=0.5)
         plt.xscale('log')
         plt.yscale('log')
-        plt.xlabel('Reynolds Number')
+        plt.xlabel('Reynolds Number (/1E6)')
         plt.ylabel('Drag Coefficient (CD)')
         plt.title('Comparison of True and Predicted CD values')
         plt.legend()
+
+        plt.savefig(figure_save_path, dpi=300, bbox_inches='tight')
         plt.show()
 
     return mse, mae

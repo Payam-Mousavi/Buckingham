800c0fcbbda5dfcc81683f1c1d4211d02ab01751
Payam Mousavi <payam.mousavi@amii.ca>
2024-03-10 16:40:41 -0700

    Finalize visualize.py to include sampling efficiency. Also switched the sampling to be in log space for better coverage.

--- visualize.py
+++ visualize.py
@@ -1,40 +1,17 @@
 
-
-
-
 from utils import *
-from models import Model_II, Model_0
-from datasets import *
-from train import train_model
-from predict import predict_model
-from eval import eval_model
-
 import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from matplotlib.patches import Rectangle
-
-import seaborn as sns
 import numpy as np
-import math
-from tqdm import tqdm
-import pandas as pd
-
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torch.utils.data import Dataset
-from torch.utils.data import DataLoader, random_split
-from torch.optim import Adam
-import torch.nn.init as init
-from torch.utils.tensorboard import SummaryWriter
 
 matplotlib.rcParams.update({'font.size': 18})
-eps = 1e-12
 
 
-BIN_SIZE = 200
-SCALE_FACTOR = 50
+BIN_SIZE = 500
+SCALE_FACTOR = 100
+SAMPLING_METHOD = "uniform-Re" # "unform-Re" OR "uniform-4"
 
 def update(frame, bins, bin_counts, scatter_dict, hist):
     rho_range = [200, 3000]
@@ -42,36 +19,68 @@ def update(frame, bins, bin_counts, scatter_dict, hist):
     D_range = [0.5, 1]
     U_range = [10, 20]
     
-    # Sample from the input parameters
-    rho = np.random.uniform(rho_range[0], rho_range[1])
-    U = np.random.uniform(U_range[0], U_range[1])
-    D = np.random.uniform(D_range[0], D_range[1])
-    mu = np.random.uniform(mu_range[0], mu_range[1])
-    
-    # Calculate Re and determine the corresponding bin
-    Re = (rho * U * D) / mu
+    if SAMPLING_METHOD == "uniform-4":
+         # Sample uniformly from 4 input parameters
+        # rho = np.random.uniform(rho_range[0], rho_range[1])
+        # U = np.random.uniform(U_range[0], U_range[1])
+        # D = np.random.uniform(D_range[0], D_range[1])
+        # mu = np.random.uniform(mu_range[0], mu_range[1])
+        log_rho = np.random.uniform(np.log10(rho_range[0]), np.log10(rho_range[1]))
+        log_U = np.random.uniform(np.log10(U_range[0]), np.log10(U_range[1]))
+        log_D = np.random.uniform(np.log10(D_range[0]), np.log10(D_range[1]))
+        log_mu = np.random.uniform(np.log10(mu_range[0]), np.log10(mu_range[1]))
+        
+        rho = np.power(10, log_rho)
+        U = np.power(10, log_U)
+        D = np.power(10, log_D)
+        mu = np.power(10, log_mu)
+
+        # Calculate Re and determine the corresponding bin
+        Re = (rho * U * D) / mu
+    elif SAMPLING_METHOD == "uniform-Re":
+        # Sample uniformly from Re
+        # Re = np.random.uniform(1e3, 1e7)
+        log_Re = np.random.uniform(3, 7)
+        Re = np.power(10, log_Re)
+    else: 
+        raise ValueError("Invalid sampling method")
+
     bin_index = int((Re - 1e3) // BIN_SIZE)
-    
+
     # Update the bin count and scatter plot
     if 0 <= bin_index < len(bin_counts):
-        bin_counts[bin_index] += 1
+        if SAMPLING_METHOD == "uniform-Re":
+            if bin_counts[bin_index] == 0:
+                bin_counts[bin_index] += 1
+        else:
+            bin_counts[bin_index] += 1
+
         CD_value = run_experiments(Re_vec=np.array([Re]))[0][0]
         if bin_index not in scatter_dict:
             scatter_dict[bin_index] = ax.scatter(bins[bin_index], CD_value, s=0, alpha=0.3)
+
         scatter_dict[bin_index].set_sizes(np.array([bin_counts[bin_index] * SCALE_FACTOR]))
     
     # Update the histogram
     hist.set_data(bins[:-1], bin_counts)
     ax_hist.set_ylim(0, max(bin_counts) * 1.1)  # Adjust the upper limit as needed
-    ax.set_title(f"Sample Count: {frame + 1}/{num_samples}")
+
+
+    repetitive_sampled_bins = bin_counts[bin_counts > 1]
+    excess_samples = np.sum(repetitive_sampled_bins - 1)
+    sampling_efficiency = 1 - (excess_samples / num_samples)
+    # print(f"Sampling Efficiency: {sampling_efficiency * 100:.2f}%")
+
+    ax.set_title(f"Sample Count: {frame + 1}/{num_samples}, Sampling Efficiency: {sampling_efficiency * 100:.2f}%")
     
     return list(scatter_dict.values()) + [hist]
 
 if __name__ == "__main__":
     fig = plt.figure(figsize=(12, 8))  # Adjust the figure size as needed
+    plt.clf()
     ax = fig.add_subplot(211)
     ax.set_xlim(1e3, 1e7)  # Adjust the x-axis range as needed
-    ax.set_ylim(0.1e-1, 5e-1)  # Adjust the y-axis range as needed
+    ax.set_ylim(0.1e-1, 6e-1)  # Adjust the y-axis range as needed
     ax.set_xscale('log')
     # ax.set_xlabel('Re')
     ax.set_ylabel('CD')
@@ -98,13 +107,22 @@ if __name__ == "__main__":
     
     # Create the histogram plot using a line plot
     hist, = ax_hist.plot(bins[:-1], bin_counts, drawstyle='steps-post')
+    ax_hist.axhline(y=1.01, color='r', linestyle='--', linewidth=2)
     
     # Create the animation
-    num_samples = 100  # Total number of samples to be drawn
+    num_samples = 200  # Total number of samples to be drawn
     animation = FuncAnimation(fig, update, frames=num_samples, fargs=(bins, bin_counts, scatter_dict, hist),
-                              interval=100)
+                              interval=100, repeat=False)
   
-    animation.save("Figures/sampling_four-inputs.mp4", writer="ffmpeg", fps=10)
+    file_path = f"Figures/sampling_{SAMPLING_METHOD}.mp4"
+    animation.save(file_path, writer="ffmpeg", fps=10)
 
     plt.tight_layout()  # Adjust the spacing between subplots
     plt.show()
+
+    # Calculating and printing sampling efficiency:
+    repetitive_sampled_bins = bin_counts[bin_counts > 1]
+    excess_samples = np.sum(repetitive_sampled_bins - 1)
+    print(f"Excess Samples: {excess_samples}")
+    sampling_efficiency = 1 - (excess_samples / num_samples)
+    print(f"Sampling Efficiency: {sampling_efficiency * 100:.2f}%")
